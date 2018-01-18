034633945bbcb90000e7af534cf1e15cb5c0e3d4
KBI <bibollus1@gmail.com>
2018-01-18 18:24:24 +0100

    401 authorization issue

--- server/models/users.js
+++ server/models/users.js
@@ -49,34 +49,46 @@ var UserSchema = new mongoose.Schema({
   }
 );
 // to not give everything back to user - fe in postman to not send password and tokens
-UserSchema.methods.toJSON = function (){
+UserSchema.methods.toJSON = function () {
   var user = this;
   var userObject = user.toObject();
 
-  return _.pick(userObject,['_id', 'email']);
+  return _.pick(userObject, ['_id', 'email']);
 };
 
-// dodawanie dowolnych metod
-// arrow function do not bind this keyword!
-UserSchema.methods.generateAuthToken = function() {
+UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({
-    _id: user._id.toHexString(),
-    access
-  }, 'abcd1234').toString();
-  user.tokens.push({
-    access,
-    token
-  });
-  // it returns promise
-  // returning value passed as success argument for next call
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens.push({access, token});
+
   return user.save().then(() => {
     return token;
   });
 };
 
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
 var User = mongoose.model('User', UserSchema);
+
+
+
 
 // POST /users
 
--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ const {
 const {
   User
 } = require('./models/users.js')
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 // bodyparser take json and converts to body object
@@ -138,30 +139,23 @@ app.patch('/todos/:id', (req, res) => {
 ////////////////Authentication
 ///////Start
 
-app.post('/users', (req, res)=>{
+app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
-  user.save().then((user)=>{
+  user.save().then(() => {
     return user.generateAuthToken();
-  }).then((token)=>{
+  }).then((token) => {
     res.header('x-auth', token).send(user);
-  }).catch((e)=>{
+  }).catch((e) => {
     res.status(400).send(e);
-    console.log("User with this email already exist!");
   })
 });
 
-app.get('/users', (req, res) => {
-  // return everything
-  Todo.find().then((users) => {
-    res.send({
-      users
-    }); // making code more flexible for future
-  }, (e) => {
-    res.status(400).send(e);
-  });
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
+
 
 app.listen(port, () => {
   console.log(`Started on port ${port}! `);
